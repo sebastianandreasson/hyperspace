e45a4638e0ba168b7763aa1dbf106d565e75285b
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2015-04-23 11:14:35 +0200

    unzip up n running

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var unzip = require('node-unzip');
+var fs = require('fs');
+var exec = require('child_process').exec;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,19 +22,51 @@ app.set('view engine', 'hjs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(multer({
+    dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename + Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+        fs.createReadStream(file.path).pipe(unzip.Extract({ path: './projects/' + file.originalname.replace('.zip', '') 
+                                                          }).on('close', function(){
+            console.log('DONE!');
+            initProject(file.path);
+        }));
+    }
+}));
+
+var initProject = function(filePath){
+    var cute = 'npm install ' + filePath;
+    exec(cute, function (error, stdout, stderr) {
+        if (error || stderr){
+            console.log(error || stderr);
+        }
+        else{
+            console.log('DONE!');
+        }
+    });
+};
+
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,23 +74,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Hyperspace' });
+});
+
+router.post('/', function (req, res) {
+//    fs.readFile(req.files.displayImage.path, function (err, data) {
+//        var newPath = __dirname + "/" + req.files.displayImage.name;
+//        fs.writeFile(newPath, data, function (err) {
+//            if (err) throw err;
+//        });
+//    });
 });
 
 module.exports = router;
